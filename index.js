cc373cafeb2c2b67a3494dbbe20b69e1842fba6f
Rafael <rafaelfr@outlook.com>
2020-10-05 18:06:20 -0300

    feat: update only development dependencies option

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 const fs = require('fs')
 const path = require('path')
 const { spawn } = require('child_process')
+const { program } = require('commander')
+const { description, version } = require('./package.json')
 
 const package = require(`${process.cwd()}/package.json`)
+
+program
+  .version(version)
+  .description(description)
+  .option('-D, --dev', 'update only devDependencies')
+
+program.parse(process.argv)
+
+const args = []
+
+if (program.dev) args.push('-D')
 
 const packageManager = fs.existsSync(path.resolve(process.cwd(), 'yarn.lock'))
   ? 'yarn'
@@ -13,11 +26,15 @@ const packageManager = fs.existsSync(path.resolve(process.cwd(), 'yarn.lock'))
 const installCommand = { npm: 'install', yarn: 'add' }
 
 const dependencies = [
-  ...Object.keys(package.dependencies),
-  ...Object.keys(package.devDependencies),
+  ...Object.keys(package.dependencies || {}),
+  ...Object.keys(package.devDependencies || {}),
 ].map(dependency => `${dependency}@latest`)
 
-spawn(packageManager, [installCommand[packageManager], ...dependencies], {
-  cwd: process.cwd(),
-  stdio: 'inherit',
-})
+spawn(
+  packageManager,
+  [installCommand[packageManager], ...dependencies, ...args],
+  {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+  },
+)
