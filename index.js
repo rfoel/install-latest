bcf784108364481a4dd0049f8230e56544b9bdde
Rafael Franco <rafaelfr@outlook.com>
2022-10-03 17:02:33 -0300

    fix: refactor structure

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 #!/usr/bin/env node
 
-import { existsSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 import { resolve } from 'path'
 import inquirer from 'inquirer'
 import { spawn } from 'child_process'
 import { program } from 'commander'
 import { description, version } from './package.json'
-
-const packageFile = require(`${process.cwd()}/package.json`)
-
-const getDependencies = type =>
-  packageFile[type]
-    ? Object.entries(packageFile[type])
-        .filter(([value]) => !isNaN(value[0]))
-        .map(([key]) => key)
-    : []
 
 program
   .version(version)
@@ -31,35 +22,49 @@ program
 program.parse(process.argv)
 const options = program.opts()
 
-const dependencies = []
-
-if ([options.prod, options.dev, options.optional, options.peer].some(Boolean)) {
-  if (options.prod) dependencies.push(...getDependencies('dependencies'))
-  if (options.dev) dependencies.push(...getDependencies('devDependencies'))
-  if (options.optional)
-    dependencies.push(...getDependencies('optionalDependencies'))
-  if (options.peer) dependencies.push(...getDependencies('peerDependencies'))
-} else {
-  dependencies.push(
-    ...getDependencies('dependencies'),
-    ...getDependencies('devDependencies'),
-    ...getDependencies('optionalDependencies'),
-    ...getDependencies('peerDependencies'),
-  )
-}
-
 ;(async () => {
+  packageFile = JSON.parse(
+    readFileSync(`${process.cwd()}/package.json`).toString(),
+  )
+
+  const getDependencies = type =>
+    packageFile[type]
+      ? Object.entries(packageFile[type])
+          .filter(([, value]) => !isNaN(value[0]))
+          .map(([key]) => key)
+      : []
+
+  let dependencies = []
+
+  if (
+    [options.prod, options.dev, options.optional, options.peer].some(Boolean)
+  ) {
+    if (options.prod) dependencies.push(...getDependencies('dependencies'))
+    if (options.dev) dependencies.push(...getDependencies('devDependencies'))
+    if (options.optional)
+      dependencies.push(...getDependencies('optionalDependencies'))
+    if (options.peer) dependencies.push(...getDependencies('peerDependencies'))
+  } else {
+    dependencies.push(
+      ...getDependencies('dependencies'),
+      ...getDependencies('devDependencies'),
+      ...getDependencies('optionalDependencies'),
+      ...getDependencies('peerDependencies'),
+    )
+  }
+
+  console.log(dependencies)
+
   if (options.interactive) {
     await inquirer
       .prompt({
         type: 'checkbox',
-        name: 'userPackages',
+        name: 'selectedPackages',
         message: 'Select the packages to be updated:',
         choices: [...dependencies],
       })
       .then(answers => {
-        dependencies.length = 0
-        dependencies.push(...answers.userPackages)
+        dependencies = answers.selectedPackages
       })
   }
 
