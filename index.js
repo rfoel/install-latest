2e753059ab457f3798ea707916c65f57de8d27ea
Rafael Franco <rafaelfr@outlook.com>
2025-09-26 12:57:05 -0300

    chore: make package lighter
    BREAKING CHANGE: some packages were removed

--- index.js
+++ index.js
@@ -1,90 +1,104 @@
 #!/usr/bin/env node
 
-import { existsSync, readFileSync } from 'fs'
-import { resolve } from 'path'
-import inquirer from 'inquirer'
-import { spawn } from 'child_process'
-import { program } from 'commander'
-import { description, version } from './package.json'
+import { existsSync, readFileSync } from "fs";
+import { resolve } from "path";
+import prompts from "prompts";
+import { spawn } from "child_process";
+import { description, version } from "./package.json";
 
-program
-  .version(version)
-  .description(description)
-  .option('--prod', 'update only production dependencies')
-  .option('--dev', 'update only development dependencies')
-  .option('--optional', 'update only optional dependencies')
-  .option('--peer', 'update only peer dependencies')
-  .option(
-    '-i, --interactive',
-    'interactively select the dependency types you want to update',
-  )
+// Simple CLI argument parsing
+const args = process.argv.slice(2);
+const options = {
+  prod: args.includes("--prod"),
+  dev: args.includes("--dev"),
+  optional: args.includes("--optional"),
+  peer: args.includes("--peer"),
+  interactive: args.includes("-i") || args.includes("--interactive"),
+  help: args.includes("-h") || args.includes("--help"),
+  version: args.includes("-V") || args.includes("--version"),
+};
 
-program.parse(process.argv)
-const options = program.opts()
+if (options.help) {
+  console.log(`${description}
 
-;(async () => {
+Usage: install-latest [options]
+
+Options:
+  -V, --version     output the version number
+  --prod            update only production dependencies
+  --dev             update only development dependencies
+  --optional        update only optional dependencies
+  --peer            update only peer dependencies
+  -i, --interactive interactively select the dependency types you want to update
+  -h, --help        display help for command`);
+  process.exit(0);
+}
+
+if (options.version) {
+  console.log(version);
+  process.exit(0);
+}
+
+(async () => {
   packageFile = JSON.parse(
     readFileSync(`${process.cwd()}/package.json`).toString(),
-  )
+  );
 
-  const getDependencies = type =>
+  const getDependencies = (type) =>
     packageFile[type]
       ? Object.entries(packageFile[type])
           .filter(([, value]) => isNaN(value[0]))
           .map(([key]) => key)
-      : []
+      : [];
 
-  let dependencies = []
+  let dependencies = [];
 
   if (
     [options.prod, options.dev, options.optional, options.peer].some(Boolean)
   ) {
-    if (options.prod) dependencies.push(...getDependencies('dependencies'))
-    if (options.dev) dependencies.push(...getDependencies('devDependencies'))
+    if (options.prod) dependencies.push(...getDependencies("dependencies"));
+    if (options.dev) dependencies.push(...getDependencies("devDependencies"));
     if (options.optional)
-      dependencies.push(...getDependencies('optionalDependencies'))
-    if (options.peer) dependencies.push(...getDependencies('peerDependencies'))
+      dependencies.push(...getDependencies("optionalDependencies"));
+    if (options.peer) dependencies.push(...getDependencies("peerDependencies"));
   } else {
     dependencies.push(
-      ...getDependencies('dependencies'),
-      ...getDependencies('devDependencies'),
-      ...getDependencies('optionalDependencies'),
-      ...getDependencies('peerDependencies'),
-    )
+      ...getDependencies("dependencies"),
+      ...getDependencies("devDependencies"),
+      ...getDependencies("optionalDependencies"),
+      ...getDependencies("peerDependencies"),
+    );
   }
 
   if (options.interactive) {
-    await inquirer
-      .prompt({
-        type: 'checkbox',
-        name: 'selectedPackages',
-        message: 'Select the packages to be updated:',
-        choices: [...dependencies],
-      })
-      .then(answers => {
-        dependencies = answers.selectedPackages
-      })
+    const response = await prompts({
+      type: "multiselect",
+      name: "selectedPackages",
+      message: "Select the packages to be updated:",
+      choices: dependencies.map(dep => ({ title: dep, value: dep })),
+    });
+    dependencies = response.selectedPackages || [];
   }
 
-  const packageManager = existsSync(resolve(process.cwd(), 'yarn.lock'))
-    ? 'yarn'
-    : 'npm'
+  const packageManager = existsSync(resolve(process.cwd(), "yarn.lock"))
+    ? "yarn"
+    : "npm";
 
   const executableName = /^win/.test(process.platform)
-    ? packageManager + '.cmd'
-    : packageManager
+    ? packageManager + ".cmd"
+    : packageManager;
 
-  const installCommand = { npm: 'install', yarn: 'add' }
+  const installCommand = { npm: "install", yarn: "add" };
 
   spawn(
     executableName,
     [
       installCommand[packageManager],
-      ...dependencies.map(dependency => `${dependency}@latest`),
+      ...dependencies.map((dependency) => `${dependency}@latest`),
     ],
     {
       cwd: process.cwd(),
-      stdio: 'inherit',
+      stdio: "inherit",
     },
-  )
-})()
+  );
+})();
